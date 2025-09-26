d51b37e212987244c01b62ebff61be9a1a16306b
l96n <l96n@proton.me>
2025-09-26 02:27:52 -1000

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,18 +9,15 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
 
-
     def test_subtract(self):
         self.assertEqual(subtract(1, 2), -1)
         self.assertEqual(subtract(-1, 1), -2)
         self.assertEqual(subtract(-1, -1), 0)
 
-
     def test_multiply(self):
         self.assertEqual(multiply(1, 2), 2)
         self.assertEqual(multiply(-1, 1), -1)
         self.assertEqual(multiply(-1, -1), 1)
-
 
     def test_divide(self):
         self.assertEqual(divide(1, 2), 0.5)
